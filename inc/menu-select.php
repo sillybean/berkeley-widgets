6a114282fceac68d0b1aaec10dbaaa0a9cfa99cb
Stephanie Leary <steph@sillybean.net>
2016-03-14 12:30:53 -0500

    add tax list widget; split files

---
+++ inc/menu-select.php
@@ -0,0 +1,122 @@
+<?php
+
+// Widget for nav menu <select> list
+
+ class WP_Nav_Menu_Dropdown_Widget extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array( 'description' => __('Add a custom menu dropdown to your sidebar.') );
+		parent::__construct( 'nav_menu_dropdown', __('Custom Menu Dropdown'), $widget_ops );
+	}
+
+
+	public function widget( $args, $instance ) {
+		// Get menu
+		$nav_menu = ! empty( $instance['nav_menu'] ) ? wp_get_nav_menu_object( $instance['nav_menu'] ) : false;
+
+		if ( !$nav_menu )
+			return;
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		echo $args['before_widget'];
+
+		if ( !empty($instance['title']) )
+			echo $args['before_title'] . $instance['title'] . $args['after_title'];
+
+		$nav_menu_args = array(
+			'fallback_cb' => '',
+			'menu'        => $nav_menu,
+			'walker'         => new Walker_Nav_Menu_Dropdown(),
+			'items_wrap'     => '<div class="select-menu"><form><select onchange="if (this.value) window.location.href=this.value">%3$s</select></form></div>',
+		);
+
+
+		wp_nav_menu( apply_filters( 'widget_nav_menu_args', $nav_menu_args, $nav_menu, $args, $instance ) );
+
+		echo $args['after_widget'];
+	}
+
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		if ( ! empty( $new_instance['title'] ) ) {
+			$instance['title'] = sanitize_text_field( stripslashes( $new_instance['title'] ) );
+		}
+		if ( ! empty( $new_instance['nav_menu'] ) ) {
+			$instance['nav_menu'] = (int) $new_instance['nav_menu'];
+		}
+		return $instance;
+	}
+
+
+	public function form( $instance ) {
+		$title = isset( $instance['title'] ) ? $instance['title'] : '';
+		$nav_menu = isset( $instance['nav_menu'] ) ? $instance['nav_menu'] : '';
+
+		// Get menus
+		$menus = wp_get_nav_menus();
+
+		// If no menus exists, direct the user to go and create some.
+		?>
+		<p class="nav-menu-widget-no-menus-message" <?php if ( ! empty( $menus ) ) { echo ' style="display:none" '; } ?>>
+			<?php
+			if ( isset( $GLOBALS['wp_customize'] ) && $GLOBALS['wp_customize'] instanceof WP_Customize_Manager ) {
+				$url = 'javascript: wp.customize.panel( "nav_menus" ).focus();';
+			} else {
+				$url = admin_url( 'nav-menus.php' );
+			}
+			?>
+			<?php echo sprintf( __( 'No menus have been created yet. <a href="%s">Create some</a>.' ), esc_attr( $url ) ); ?>
+		</p>
+		<div class="nav-menu-widget-form-controls" <?php if ( empty( $menus ) ) { echo ' style="display:none" '; } ?>>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ) ?></label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>"/>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'nav_menu' ); ?>"><?php _e( 'Select Menu:' ); ?></label>
+				<select id="<?php echo $this->get_field_id( 'nav_menu' ); ?>" name="<?php echo $this->get_field_name( 'nav_menu' ); ?>">
+					<option value="0"><?php _e( '&mdash; Select &mdash;' ); ?></option>
+					<?php foreach ( $menus as $menu ) : ?>
+						<option value="<?php echo esc_attr( $menu->term_id ); ?>" <?php selected( $nav_menu, $menu->term_id ); ?>>
+							<?php echo esc_html( $menu->name ); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</p>
+		</div>
+		<?php
+	}
+}
+
+
+
+// Walker class to display menu items in <select> list
+
+class Walker_Nav_Menu_Dropdown extends Walker_Nav_Menu {
+	
+	function start_lvl(&$output, $depth){
+		$indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
+	}
+	
+	function end_lvl(&$output, $depth){
+		$indent = str_repeat("\t", $depth); // don't output children closing tag
+	}
+
+	function start_el(&$output, $item, $depth, $args) {
+ 		$url = '#' !== $item->url ? $item->url : '';
+ 		$output .= '<option value="' . $url . '">' . $item->title;
+	}	
+	
+	function end_el(&$output, $item, $depth){
+		$output .= "</option>\n"; // replace closing </li> with the option tag
+	}
+}
+
+// helper function for the feed display widget
+function scl_not_empty( $val ) {
+	$val = str_replace( array( "\n", "\r", "\t" ), '', $val );
+    return !empty( $val );
+}
---
+++ inc/taxonomy-list.php
@@ -0,0 +1,86 @@
+<?php
+
+// Widget for taxonomy term list
+
+ class Berkeley_Taxonomy_List_Widget extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array( 'description' => __('Add a simple list of taxonomy terms to your sidebar.') );
+		parent::__construct( 'tax_term_list', __('Taxonomy List'), $widget_ops );
+	}
+
+
+	public function widget( $args, $instance ) {
+		// Get taxonomy
+		
+		$tax = ! empty( $instance['tax'] ) ? $instance['tax'] : false;
+
+		if ( !$tax || ! taxonomy_exists( $tax ) )
+			return;
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		echo $args['before_widget'];
+
+		if ( !empty($instance['title']) )
+			echo $args['before_title'] . $instance['title'] . $args['after_title'];
+
+		$tax_args = array(
+			'taxonomy'	=> $tax,
+			'title_li'	=> '',
+		);
+		//$args = apply_filters( 'widget_tax_menu_args', $args );
+
+		echo '<ul class="tax-term-list">';
+		wp_list_categories( $tax_args );
+		echo '</ul>';
+
+		echo $args['after_widget'];
+	}
+
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		if ( ! empty( $new_instance['title'] ) ) {
+			$instance['title'] = sanitize_text_field( stripslashes( $new_instance['title'] ) );
+		}
+		if ( ! empty( $new_instance['tax'] ) ) {
+			$instance['tax'] = sanitize_text_field( stripslashes( $new_instance['tax'] ) );
+		}
+		return $instance;
+	}
+
+
+	public function form( $instance ) {
+		$title = isset( $instance['title'] ) ? $instance['title'] : '';
+		$tax = isset( $instance['tax'] ) ? $instance['tax'] : '';
+
+		$args = array(
+		  'public'   => true
+		); 
+		$output = 'objects'; // names or objects
+		$operator = 'and'; // 'and' or 'or'
+		$taxonomies = get_taxonomies( $args, $output, $operator );
+
+		?>
+		<div class="tax-list-widget-form-controls" <?php if ( empty( $taxonomies ) ) { echo ' style="display:none" '; } ?>>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ) ?></label>
+				<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>"/>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id( 'tax' ); ?>"><?php _e( 'Select Taxonomy:' ); ?></label><br>
+				<select id="<?php echo $this->get_field_id( 'tax' ); ?>" name="<?php echo $this->get_field_name( 'tax' ); ?>" class="widefat">
+					<option value="0"><?php _e( '&mdash; Select &mdash;' ); ?></option>
+					<?php foreach ( $taxonomies as $taxonomy ) : ?>
+						<option value="<?php echo esc_attr( $taxonomy->name ); ?>" <?php selected( $tax, $taxonomy->name ); ?>>
+							<?php echo esc_html( $taxonomy->label ); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</p>
+		</div>
+		<?php
+	}
+}
